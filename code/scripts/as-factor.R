5676510e972f131c6d99333920a8bd20987c3ddd
Abby Vogel <abbyvogel@berkeley.edu>
2016-11-03 21:33:27 -0700

    bug: fixed as factor

--- code/scripts/as-factor.R
+++ code/scripts/as-factor.R
@@ -5,4 +5,4 @@ temp_credit <- model.matrix(Balance ~ ., data = credit)
 new_credit <- cbind(temp_credit[,-1], Balance = credit$Balance)
 
 scaled_credit <- scale(new_credit, center= TRUE, scale= TRUE)
-write.csv(new_credit, file = "../../data/cred_f.csv")
+write.csv(scaled_credit, file = "../../data/cred_f.csv")
